cbe7627e6c3e64c9bccb1ff1ca42519156f6db1e
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2022-06-08 13:08:25 +0300

    fix pagination

--- pagination.php
+++ pagination.php
@@ -2,21 +2,25 @@
     <ul class="pagination justify-content-center">
         <?php if ($total_pages > 1): ?>
             <li class="page-item">
-                <a class="page-link" href="?page=<?php echo '1' . '&limit=' . $limit . isset($params) ?>">Перша</a>
+                <a class="page-link" href="?page=<?php echo '1' . '&limit=' . $limit;
+                if(isset($params)) echo $params ?>">Перша</a>
             </li>
             <?php if ($page > 1): ?>
                 <li class="page-item">
-                    <a class="page-link" href="?page=<?php echo ($page - 1) . '&limit=' . $limit . isset($params) ?>">Попередня</a>
+                    <a class="page-link" href="?page=<?php echo ($page - 1) . '&limit=' . $limit ;
+                                                    if(isset($params)) echo $params?>">Попередня</a>
                 </li>
             <?php endif; ?>
             <?php if ($page < $total_pages): ?>
                 <li class="page-item">
-                    <a class="page-link" href="?page=<?php echo ($page + 1) . '&limit=' . $limit . isset($params) ?>">Наступна</a>
+                    <a class="page-link" href="?page=<?php echo ($page + 1) . '&limit=' . $limit;
+                                                    if(isset($params)) echo $params?>">Наступна</a>
                 </li>
             <?php endif; ?>
 
             <li class="page-item">
-                <a class="page-link" href="?page=<?php echo $total_pages . '&limit=' . $limit . isset($params) ?>">Остання</a>
+                <a class="page-link" href="?page=<?php echo $total_pages . '&limit=' . $limit ;
+                                                if(isset($params)) echo $params ?>">Остання</a>
             </li>
         <?php endif; ?>
     </ul>
